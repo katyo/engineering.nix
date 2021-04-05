2ac7089d76105986b1c1eff4b6c89d417716387b
K <kayo@illumium.org>
2021-04-05 13:05:54 +0500

    Update cura package

--- default.nix
+++ default.nix
@@ -3,16 +3,29 @@ self: super: with super; rec {
     packageOverrides = pySelf: pySuper: {
       trimesh = pySuper.callPackage ./cura/python-modules/trimesh {};
       uranium = pySuper.callPackage ./cura/python-modules/uranium {};
+      pynest2d = pySuper.callPackage ./cura/python-modules/pynest2d {};
     };
   };
 
-  curaengine_latest = callPackage ./cura/engine { inherit (python3.pkgs) libarcus; };
-  cura_latest = qt5.callPackage ./cura { curaengine = curaengine_latest; };
+  libnest2d = callPackage ./cura/libraries/libnest2d {};
+
+  curaengine_latest = callPackage ./cura/engine {
+    inherit (python3.pkgs) libarcus;
+  };
+  cura_latest = qt5.callPackage ./cura {
+    curaengine = curaengine_latest;
+    plugins = with curaPlugins_latest; [ octoprint ];
+  };
   curaPlugins_latest = callPackage ./cura/plugins.nix { };
 
   horizon-eda = callPackage ./horizon {};
 
   librepcb = libsForQt5.callPackage ./librepcb {};
+
+  trilinos = callPackage ./trilinos { lapack = liblapack; };
+  xyce = callPackage ./xyce { inherit trilinos; lapack = liblapack; };
+
+  qucs = callPackage ./qucs {};
 
   fungw = callPackage ./pcb-rnd/fungw {
     lua = lua5_1;
